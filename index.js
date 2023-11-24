0360e3038716913fb9d16b203f58823a95ca703e
Кирилл Залогин <hardrockman@mail.ru>
2023-11-24 23:20:15 +0900

    update .gitignore and index.js

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ console.log(_.intersection([2, 1], [2, 3])); // => [2]
 
 // capitalize делает первую букву заглавной
 console.log(_.capitalize("hello")); // Hello
+
+console.log(_.last(['one', 'two']));
