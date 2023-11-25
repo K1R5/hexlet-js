22aa9f0fc3300efcf13757e7ccc8177038ceb1cc
Кирилл Залогин <hardrockman@mail.ru>
2023-11-25 22:34:25 +0900

    update index and package

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// По общепринятому соглашению, lodash импортируется под именем _
-import _ from "lodash";
+const fn   = (a, b) => {
 
-// Пересечение массивов
-console.log(_.intersection([2, 1], [2, 3])); // => [2]
-
-// capitalize делает первую букву заглавной
-console.log(_.capitalize("hello")); // Hello
-
-console.log(_.last(['one', 'two']));
+    const c = a + b; return c;
+}
